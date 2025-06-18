6324e3c9f81c7dd0f710bf097db1145b01511a4f
bahiya666 <126662308+bahiya666@users.noreply.github.com>
2025-06-18 17:52:39 +0200

    all tests pass

--- snap-vision/jest.config.js
+++ snap-vision/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
-  transformIgnorePatterns: [
+ transformIgnorePatterns: [
     'node_modules/(?!(react-native'
     + '|@react-native'
     + '|@react-navigation'
@@ -10,12 +10,17 @@ module.exports = {
     + '|react-clone-referenced-element'
     + '|react-native-svg'
     + '|native-base'
+    + '|react-native-tts'
+    + '|expo-font'
+    + '|react-native-vector-icons'
     + ')/)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',         // supports paths like `~/screens/...`
     '^app/\\(tabs\\)/(.*)$': '<rootDir>/app/(tabs)/$1', // optional, if you still use that folder
+   '^@expo/vector-icons/?(.*)$': 'react-native-vector-icons/$1',
+    //'^expo-font$': '<rootDir>/__mocks__/expo-font.js',
   },
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 };
