786b82216154704fe9858035ff2d7d6b5dae860d
lekishachetty <lchetty1504@gmail.com>
2025-06-24 19:44:58 +0200

    firebase emulators

--- snap-vision/firebase.js
+++
@@ -1,7 +0,0 @@
-// firebase.js
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-
-// No need for manual initialization — already auto-initialized via native setup
-
-export { auth, firestore };
--- snap-vision/jest.config.js
+++ snap-vision/jest.config.js
@@ -1,37 +1,30 @@
 module.exports = {
   preset: 'jest-expo',
- transformIgnorePatterns: [
-    'node_modules/(?!(react-native'
-    + '|@react-native'
-    + '|@react-navigation'
-    + '|expo(nent)?'
-    + '|@expo(nent)?'
-    + '|expo-modules-core'
-    + '|react-clone-referenced-element'
-    + '|react-native-svg'
-    + '|native-base'
-    + '|react-native-tts'
-    + '|expo-font'
-    + '|react-native-vector-icons'
-    + ')/)',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-firebase|@react-navigation|expo(nent)?|@expo(nent)?|expo-modules-core|react-clone-referenced-element|react-native-svg|native-base|react-native-tts|expo-font|expo-asset|react-native-vector-icons)/',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['<rootDir>/__tests__/helpers/setup-jest.js'],
   moduleNameMapper: {
-    '^~/(.*)$': '<rootDir>/src/$1',         // supports paths like `~/screens/...`
+    '^~/(.*)$': '<rootDir>/src/$1',
     '^app/\\(tabs\\)/(.*)$': '<rootDir>/app/(tabs)/$1', 
-   '^@expo/vector-icons/?(.*)$': 'react-native-vector-icons/$1',
-    //'^expo-font$': '<rootDir>/__mocks__/expo-font.js',
+    '^@expo/vector-icons$': '<rootDir>/__mocks__/@expo/vector-icons/index.js',
+    '^@expo/vector-icons/(.*)$': '<rootDir>/__mocks__/@expo/vector-icons/$1',
+    '^react-native-vector-icons/(.*)$': '<rootDir>/__mocks__/react-native-vector-icons/$1'
   },
-  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-
-  collectCoverage: false, // Set to true if you want to collect coverage
-
+  testMatch: ['**/__tests__/**/*.test.ts?(x)', '<rootDir>/detox/tests/**/*.test.js'],
+  collectCoverage: false,
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
-    '!**/__tests__/**',         
-    '!**/index.{ts,tsx}',        
-    '!**/*.d.ts',                
+    '!**/__tests__/**',
+    '!**/index.{ts,tsx}',
+    '!**/*.d.ts',
   ],
-
   coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+      presets: ['babel-preset-expo'],
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    }]
+  }
 };
