bd20846a98f36ba9fae9516c7efbbf9ca9aea049
tishreddy <u19072211@tuks.co.za>
2025-06-25 23:03:11 +0200

    Updated Screens and Content

--- snap-vision/src/screens/MapScreen.tsx
+++ snap-vision/src/screens/MapScreen.tsx
@@ -29,7 +29,7 @@ type MapScreenParams = {
   lng?: string;
 };
 
-const ROUTING_API_BASE = "http://10.0.2.2:3000"; // <-- Use your correct backend IP here
+const ROUTING_API_BASE = "http://192.168.0.133:3000"; // <-- Use your correct backend IP here
 // emulator: 10.0.2.2
 // B home:  192.168.56.1
 // L wifi: 192.168.0.127
@@ -76,7 +76,7 @@ const MapScreen = () => {
   const [pois, setPOIs] = useState<any[]>([]);
   const [poiSuggestions, setPOISuggestions] = useState<any[]>([]);
 
-  // share location things
+  // share location
   const route = useRoute();
   const params = route.params as MapScreenParams;
   const [hasHandledDeepLink, setHasHandledDeepLink] = useState(false);
