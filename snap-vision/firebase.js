3df48c8d4de0807e46b69571d960a55206b386c4
tishreddy <u19072211@tuks.co.za>
2025-08-14 21:11:01 +0200

    Prepare backend for production deployment

--- snap-vision/firebase.js
+++ snap-vision/firebase.js
@@ -2,8 +2,8 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
-// Connect to Firebase Emulator if running in a test environment
-if (__DEV__) {
+// Connect to Firebase Emulator ONLY in development AND when not using production backend
+if (__DEV__ && !process.env.FIREBASE_USE_PRODUCTION) {
   firestore().useEmulator('127.0.0.1', 8080);
   auth().useEmulator('http://127.0.0.1:9099');
 }
