16200a1b0a6d38967d7d7e9a4b6feff8f1bfad4c
tishreddy <u19072211@tuks.co.za>
2025-08-19 21:54:14 +0200

    fixed lint display name

--- snap-vision/__tests__/integration/mapContent.integration.test.tsx
+++ snap-vision/__tests__/integration/mapContent.integration.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import { ThemeProvider } from '../../src/theme/ThemeContext';
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
